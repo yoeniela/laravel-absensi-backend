f1a1f748e614aa3222f6193e045b6fdcd4b5ca1d
Yhonho <yoeniela@gmail.com>
2024-09-30 18:53:24 +0800

    company dashboard & api

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
@@ -26,6 +27,7 @@ class User extends Authenticatable
         'department',
         'face_embedding',
         'image_url',
+        'fcm_token'
     ];
 
     /**
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -19,6 +20,23 @@ class DatabaseSeeder extends Seeder
             'name' => 'yhonho',
             'email' => 'yhonho@gmail.com',
             'password' => Hash::make('qwerasdf'),
+        ]);
+
+        // data dummy for company
+        \App\Models\Company::create([
+            'name' => 'PT. Yhonho Pharma',
+            'email' => 'yhonhopharma@gmail.com',
+            'address' => 'Jl. Kakap Sungai Dama Samarinda Kalimantan Timur',
+            'latitude' => '-0.506021',
+            'longitude' => '117.159570',
+            'radius_km' => '0.5',
+            'time_in' => '08:00',
+            'time_out' => '16:30',
+        ]);
+
+        $this->call([
+            AttendanceSeeder::class,
+            PermissionSeeder::class,
         ]);
     }
 }
